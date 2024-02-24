640d7d0b48cfb58976d65e5f8d45411d5430e18a
Matheus Esteves <matheusesantos@gmail.com>
2024-02-24 11:23:19 -0300

    using useReducer to optimize the code

--- orgsHooks/src/screens/Home/components/Producer/Producer.js
+++ orgsHooks/src/screens/Home/components/Producer/Producer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { 
     Text,
     View,
@@ -10,12 +10,15 @@ import { styles } from './styles_producer.js';
 import Stars from '../../../../components/stars.js';
 
 export default function Producer({ nome, imagem, distancia, estrelas }) {
-    const [ selected, setSelected ] = useState(false);
+    const [ selected, invertSelect ] = useReducer(
+        (selected) => !selected,
+        false
+    );
 
     return (
         <TouchableOpacity 
             style={ styles.card }
-            onPress={ () => setSelected(!selected) }
+            onPress={invertSelect}
         >
             <Image source={ imagem } acessibilityLabes={ nome } style={ styles.image }/>
 
